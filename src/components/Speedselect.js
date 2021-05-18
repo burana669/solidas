7990ca8a9c39325cb5096b14469b7c2201a26301
Matti Korhonen <burana@kapsi.fi>
2021-05-18 22:54:54 +0300

    initial plans

---
+++ src/components/Speedselect.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Speedselect = () => {
+    return <div>
+        Speedselect
+    </div>
+}
---
+++ src/components/VehicleDistance.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const VehicleDistance = () => {
+
+    return (<div>
+        <button>Auto 1</button>
+        <button>Auto 2</button>
+        <button>Auto 3</button>
+        <input></input>
+    </div>)
+}
